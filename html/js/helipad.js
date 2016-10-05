9ecf53614dadf8468774d7999559b793e0f25935
Boeckmann, Tim <boeckmt@amazon.com>
2016-10-05 17:32:16 +0100

    update form and helipad.js to include fields

--- html/js/helipad.js
+++ html/js/helipad.js
@@ -2,12 +2,40 @@ angular.module('helipadApp', [])
   .controller('helipadController', function() {
     var helipad = this;
     helipad.company = {
-  		"name": "placeholder"
+      "name": "placeholder"
     };
     
     helipad.page = {
-    	"brandName":	"Helipad.io",
-		"signUpText":	"Sign Up"
+      "brandName": "Helipad.io",
+    "signUpText": "Sign Up"
+    };
+
+    helipad.fields = {
+      "email": {
+        "enabled": true,
+        "label": "Email",
+        "required": true
+      },
+      "first_name": {
+        "enabled": true,
+        "label": "First Name",
+        "required": false
+      },
+      "last_name": {
+        "enabled": true,
+        "label": "Last Name",
+        "required": false
+      },
+      "company": {
+        "enabled": false,
+        "label": "company",
+        "required": false
+      },
+      "telephone": {
+        "enabled": false,
+        "label": "company",
+        "required": false
+      }
     };
 
   });
